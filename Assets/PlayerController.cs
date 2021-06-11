465867d3c8d720d55e22c19dc86db954bb2586d5
ColinSYang <70976943+ColinSYang@users.noreply.github.com>
2021-06-11 15:00:52 -0700

    Rough Player and Enemy Interaction (#2)
    * cube sprite
    
    * Rough Player and Enemy Interaction
    
    * Revert "Rough Player and Enemy Interaction"
    
    This reverts commit 4aec91d7992fe749a41e37df22195e72277bd6d4.
    
    * Revert "Revert "Rough Player and Enemy Interaction""
    
    This reverts commit f1e9f0decf3814a8f7b2a883b38e065b24cad714.
    
    * Update BulletHellTest.unity

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public int startingHealth = 100;
+    public int currentHealth;
+    public float speed = 5;
+    public Rigidbody2D rb;
+    public float blockTime = 1f;
+
+    string blockType = "";
+    float blockTimer = 0f;
+
+    Vector2 movement;
+
+    private void Start()
+    {
+        currentHealth = startingHealth;
+    }
+
+    private void Update()
+    {
+        if (!blockType.Equals(""))
+        {
+            blockTimer += Time.deltaTime;
+            if (blockTimer >= blockTime)
+            {
+                blockType = "";
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            PlayerBlocked("Red");
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            PlayerBlocked("Blue");
+        }
+    }
+
+    void FixedUpdate()
+    {
+        movement.x = Input.GetAxis("Horizontal");
+        movement.y = Input.GetAxis("Vertical");
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+    }
+
+    public void PlayerBlocked(string type)
+    {
+        blockType = type;
+        blockTimer = 0f;
+    }
+
+    public void TakeDamage(int damageAmount)
+    {
+        currentHealth -= damageAmount;
+        Debug.Log("Ouch! Current health: " + currentHealth);
+        if (currentHealth <= 0)
+        {
+            Debug.Log("Player Died!!!!!");
+        }
+    }
+    
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("RedEnemy"))
+        {
+            if (!blockType.Equals("Red"))
+            {
+                TakeDamage(10);
+            }
+        }
+
+        if (collision.gameObject.CompareTag("BlueEnemy"))
+        {
+            if (!blockType.Equals("Blue"))
+            {
+                TakeDamage(10);
+            }
+        }
+    }
+}
