9530e2352c55a536b1029497a4a29021bbed4afb
Kirin <auraspeed@gmail.com>
2021-06-12 20:04:45 -0400

    Added the blast used to deal with enemies

--- Assets/Scripts/ButtonController.cs
+++ Assets/Scripts/ButtonController.cs
@@ -7,12 +7,19 @@ public class ButtonController : MonoBehaviour
     public Color defaultColor;
     public Color pressedColor;
     public KeyCode keyToPress;
+    public GameObject blast;
 
+
+    private GameObject player;
+    private PlayerController p_con;
     private SpriteRenderer sr;
+    private int notes = 0;
 
     private void Start()
     {
         sr = this.GetComponent<SpriteRenderer>();
+        player = GameObject.FindGameObjectWithTag("Player");
+        p_con = player.GetComponent<PlayerController>();
     }
 
     private void Update()
@@ -20,11 +27,25 @@ public class ButtonController : MonoBehaviour
         if (Input.GetKeyDown(keyToPress))
         {
             sr.color = pressedColor;
+            if (notes == 0) 
+            {
+                LevelManager.instance.LifeBlast(blast);
+            }
         }
 
         if (Input.GetKeyUp(keyToPress))
         {
             sr.color = defaultColor;
         }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        notes++;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        notes--;
     }
 }
---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+
+    public int missDamage = 5;
+    public int lifeCost = 3;
+    public static GameObject player;
+    public static PlayerController pc;
+    Transform playerTrans;
+
+    public static LevelManager instance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        pc = player.GetComponent<PlayerController>();
+        playerTrans = GameObject.FindGameObjectWithTag("Player").transform;
+        instance = this;
+    }
+
+    public void NoteHit(GameObject blast) 
+    {
+        Instantiate(blast, playerTrans.position, playerTrans.transform.rotation);
+    }
+
+    public void LifeBlast(GameObject blast) 
+    {
+        Instantiate(blast, playerTrans.position, playerTrans.transform.rotation);
+        pc.TakeDamage(lifeCost);
+    }
+
+    public void NoteMiss() 
+    {
+        pc.TakeDamage(missDamage);
+    }
+}
